421112c9e0029d173c54afcf797c52618e97e637
jpcadinelli <jpcadinelli@gmail.com>
2025-05-25 12:02:42 -0300

    refactor: ajuste rotas axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`
 })
 
 axiosInstance.interceptors.request.use((config) => {
@@ -15,25 +15,32 @@ axiosInstance.interceptors.request.use((config) => {
 
 export class UsuarioService {
     loginUsuario(data: { email: string, password: string }) {
-        return axiosInstance.post("/api/v1/login", data)
+        return axiosInstance.post("/login", data)
     }
 
     getUsuarioLogado() {
-        return axiosInstance.get("/api/v1/usuarios/logado")
+        return axiosInstance.get("/usuarios/logado")
     }
 }
 
 export class FilmeService {
   getFilmesEmCartaz() {
-    return axiosInstance.get("/api/v1/filmes/em-cartaz")
+    return axiosInstance.get("/filmes/em-cartaz")
   }
 }
 
 export class SessaoService {
-  getSessoesEmCartaz() {
-    return axiosInstance.get("/api/v1/sessoes/em-cartaz")
+    getSessoesEmCartaz() {
+      return axiosInstance.get("/sessoes/em-cartaz")
+    }
+    getSessaoById(id: string) {
+      return axiosInstance.get(`/sessoes/${id}`)
+    }
   }
-  getSessaoById(id: string) {
-    return axiosInstance.get(`/api/v1/sessoes/${id}`)
+
+  export class IngressoService {
+    ComprarIngressos() {
+      return axiosInstance.get("/ingressos/em-cartaz")
+    }
   }
-}
+    
