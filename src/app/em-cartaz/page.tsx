93a7e1978c5becb198bdeac2b57450f05e4f038e
jpcadinelli <jpcadinelli@gmail.com>
2025-05-25 10:36:46 -0300

    refactor: alteração na label do botão

--- src/app/em-cartaz/page.tsx
+++ src/app/em-cartaz/page.tsx
@@ -68,7 +68,7 @@ export default function EmCartaz() {
   
           <div className="mt-4">
             <button className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-full text-sm font-medium transition">
-              Ver detalhes
+              Comprar Ingressos!
             </button>
           </div>
         </div>
